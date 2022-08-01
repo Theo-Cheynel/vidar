af95dccab2bc40390b202ffe49a7cb8ac200463c
ThomRIKin <thomas.r@kinetix.tech>
2022-08-01 16:52:57 -0300

    save commit

--- vidar/datasets/OverfitDataset.py
+++ vidar/datasets/OverfitDataset.py
@@ -190,6 +190,7 @@ class OverfitDataset(BaseDataset):
             # Return individual or single intrinsics
             if self.single_intrinsics is not None:
                 intrinsics = self.single_intrinsics.copy()
+                print("Single intrinsics", self.single_intrinsics)
                 intrinsics[0, :] *= sample['rgb'][0].size[0]
                 intrinsics[1, :] *= sample['rgb'][0].size[1]
                 print(intrinsics, intrinsics.shape)
