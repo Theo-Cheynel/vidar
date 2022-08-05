cf81b2972e855c55624ce4ae3c5c43008a0ee35f
root <root@RD-T4-Spot-2.5kx353s3zybudk2et5zqwifakd.ax.internal.cloudapp.net>
2022-08-05 14:02:04 +0000

    Updated inference.sh

--- inference.sh
+++ inference.sh
@@ -1,2 +1,2 @@
 git pull
-python3 -m scripts.inference.inference ./configs/papers/selfcalib/ucm_thomas.yaml /data/vidar/checkpoints/glowing-serenity-66/models/050.ckpt $1 /data/output/
+python3 -m scripts.inference.inference ./configs/papers/selfcalib/ucm_thomas.yaml /data/vidar/checkpoints/glowing-serenity-66/models/050.ckpt $1 /data/output/ export_type=npy
