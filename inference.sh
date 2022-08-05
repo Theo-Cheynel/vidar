2cb94e62c2f0b597ee7c4120a2ae5191df730a70
Theo-Cheynel <theo.cheynel@gmail.com>
2022-08-05 13:15:27 +0000

    Updated inference.sh for RD-T4-3

--- inference.sh
+++ inference.sh
@@ -1,2 +1,2 @@
 git pull
-python3 -m scripts.inference.inference ./configs/papers/selfcalib/ucm_overfit.yaml ~/Downloads/vidar/checkpoints/glowing-serenity-66/050.ckpt $1 ~/Downloads/vidar/output/
+python3 -m scripts.inference.inference ./configs/papers/selfcalib/ucm_overfit.yaml /data/vidar/checkpoints/glowing-serenity-66/050.ckpt $1 /data/vidar/output/
